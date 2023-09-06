47791766a7d659036599a1f48269312170fdc6a0
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-09-06 12:01:57 +0300

    feat: close menu on click outside the block or esc

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import PropTypes from 'prop-types';
 
 import styles from "./Dropdown.module.css";
 import arrowIcon from "./imgs/arrow.svg";
@@ -7,15 +8,39 @@ const DropdownMenu = ({ options, onSelect, defaultValue }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState(defaultValue || null);
 
+  const handleDropdownClick = () => {
+    setIsOpen(!isOpen);
+  }
+
   const handleOptionClick = (option) => {
     setSelectedOption(option);
     setIsOpen(false);
     onSelect(option);
   };
 
+  const handleCloseDropdown = useCallback((evt) => {
+    if (
+      !evt.target.closest(`.${styles.dropdownMenu}`)
+      || evt.key === 'Escape'
+    ) {
+      setIsOpen(false);
+    }
+  }, []);
+  
+  useEffect(() => {
+    if (isOpen) {
+      window.addEventListener('click', handleCloseDropdown);
+      window.addEventListener('keydown', handleCloseDropdown);
+    } else {
+      window.removeEventListener('click', handleCloseDropdown);
+      window.removeEventListener('keydown', handleCloseDropdown);
+    }
+
+  }, [isOpen, handleCloseDropdown]);
+
   return (
     <div className={styles.dropdownMenu}>
-      <button className={styles.dropdown} onClick={() => setIsOpen(!isOpen)}>
+      <button className={styles.dropdown} onClick={handleDropdownClick}>
         <span>{selectedOption || "Select an option"}</span>
         <img src={arrowIcon} className={styles.arrow} style={{transform: `rotateZ(${isOpen ? "90deg" : 0})`}} alt="" />
       </button>
@@ -29,6 +54,12 @@ const DropdownMenu = ({ options, onSelect, defaultValue }) => {
       </ul>
     </div>
   );
+};
+
+DropdownMenu.propTypes = {
+  options: PropTypes.array,
+  onSelect: PropTypes.func,
+  defaultValue: PropTypes.string,
 };
 
 export default DropdownMenu;
